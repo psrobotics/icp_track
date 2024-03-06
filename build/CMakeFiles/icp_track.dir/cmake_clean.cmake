c51dbfea99a54a7b3c52dd60975a49a3c80b6760
psrobotics <psrobotics@foxmail.com>
2024-03-06 14:40:00 -0800

    add: 0306 cmake

---
+++ build/CMakeFiles/icp_track.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp_track.dir/src/icp.cpp.o"
+  "CMakeFiles/icp_track.dir/src/test.cpp.o"
+  "icp_track"
+  "icp_track.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp_track.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
